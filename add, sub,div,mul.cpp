a709557a010962206aec3e1638e29cb7ce6eba03
hafsamulla38 <164649901+hafsamulla38@users.noreply.github.com>
2024-03-30 19:56:01 +0530

    Create add, sub,div,mul.cpp

---
+++ add, sub,div,mul.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"enter two numbers:";
+cin>>a>>b;
+int sum=a+b;
+int difference=a-b;
+int product=a*b;
+float quotient=(float)a/b;
+cout<<"Sum:"<<sum<<endl;
+cout<<"Difference:"<<difference<<endl;
+cout<<"Product:"<<product<<endl;
+cout<<Quotient:"<<quotient<<endl;
+return 0;
+}
+
