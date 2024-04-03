5257c4279b2a0ea912057c3a54369ab0aaa87c28
hafsamulla38 <164649901+hafsamulla38@users.noreply.github.com>
2024-04-03 22:13:12 +0530

    Add files via upload

---
+++ 1D array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i=0;
+	int marks[5];
+	marks[0]=80;
+	marks[1]=60;
+	marks[2]=70;
+	marks[3]=85;
+	marks[4]=75;
+	for(i=0;i<5;i++)
+	{
+cout<<marks[i]<<endl;
+}
+return 0;
+}
