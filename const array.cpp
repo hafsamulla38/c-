a7338963a0c16add42a75d57ae79bfee307b70e3
hafsamulla38 <164649901+hafsamulla38@users.noreply.github.com>
2024-04-04 20:37:55 +0530

    Add files via upload

---
+++ const array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i=0;
+	int marks[5]={20,30,40,50,60};
+	{
+		for(i=0;i<5;i++)
+		{
+			cout<<marks[i]<<endl;
+		}
+	}
+	return 0;
+	
+}
