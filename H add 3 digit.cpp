cb8c77f1e04447d869535f4dd7203cf6065af5dc
Hafsa Bashir Mulla <164649901+hafsamulla38@users.noreply.github.com>
2024-04-05 21:46:33 +0530

    Add files via upload

---
+++ H add 3 digit.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    int a=1,b=2,c=7,d;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    d=a+b+c;
+    cout<<d<<endl;
+    return 0;
+    
+    
+}
